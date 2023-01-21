846424413a48c0eb8c729b8c8947ae7fa2c119b8
Alex <galvan.alex121@gmail.com>
2023-01-21 10:22:18 -0500

    theme toggle

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,24 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
+import { useState } from 'react'
 function NavBar() {
+    const [theme, setTheme] = useState<boolean>(false)
+
+    const changeTheme = (themeState:boolean) => {
+        if (themeState == true){
+            setTheme(false)
+        } else {
+            setTheme(true)
+        }
+        console.log(theme)
+    }
     return (
         <div className='sticky top-0 z-50'>
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
-                    <div className="dropdown">
+                    <div className="dropdown fluid">
                         <label tabIndex={0} className="btn btn-ghost btn-circle">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </label>
@@ -17,6 +30,7 @@ function NavBar() {
                     </div>
                 </div>
                 <div className="navbar-end">
+                    <input onChange={() => changeTheme(theme)} type="checkbox" className="toggle toggle-info rounded-xl" />
                     <Link className="btn btn-ghost normal-case text-xl" href='/'>AG</Link>
                 </div>
 
