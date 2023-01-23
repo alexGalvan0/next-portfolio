a6e958bbdb0e25c2885b59d199da6623cbe43aa4
Alex <galvan.alex121@gmail.com>
2023-01-23 14:57:02 -0500

    image

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,7 @@ function About() {
         <div id="about" className='pt-11 flex justify-center'>
             <div className="bg-base-200 rounded-md">
                 <div className="hero-content flex-col lg:flex-row max-w-4xl">
-                    <Image src="/../public/images/galvan-alex.jpeg" width={300} height={200} alt="alex galvan" className="max-w-sm rounded-lg shadow-2xl" />
+                    <Image src="/images/galvan-alex.jpeg" width={300} height={200} alt="alex galvan" className="max-w-sm rounded-lg shadow-2xl" />
                     <div className='sm:text-center lg:text-start'>
                         <h1 className="text-5xl font-bold">About Me</h1>
                         <p className="py-6">My name is Alex Galvan and I have been
