7944f975ae8ca657d3f0eca1428e71c36a5b247b
Alex <galvan.alex121@gmail.com>
2023-01-23 16:33:47 -0500

    favicon

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Alex Galvan</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Alex Galvan Full Stack Developer" />
+      <meta name="description" content="Alex Galvan" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
