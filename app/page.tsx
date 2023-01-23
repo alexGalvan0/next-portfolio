62c8cb7b68a8fcbe4b25ce2a3c90c91e3b38f3d9
Alex <galvan.alex121@gmail.com>
2023-01-22 22:42:08 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,17 @@ import { useState, useEffect } from 'react'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import NavBar from '../components/NavBar'
+import Card from '../components/Card'
+import { projects } from '../projects'
+
+
+type projectt = {
+  imagePath: string
+  title: string
+  description: string
+  liveDemo: string
+  repoLink: string
+}
 
 export default function Home() {
 
@@ -19,18 +30,55 @@ export default function Home() {
       <NavBar />
       <Hero />
       <About />
-      <div className="flex
+      {/* <div className="flex
                       md:flex-row md:justify-evenly 
                       sm:gap-10 sm:justify-center sm:flex-col sm:pl-10
                       pt-10 bg-base-200 rounded-md 
                       py-10 mx-20 mt-10">
+                        
         <div className="badge badge-lg">React</div>
         <div className="badge badge-lg badge-primary">NextJs</div>
         <div className="badge badge-lg badge-secondary">Typescript</div>
         <div className="badge badge-lg badge-accent">Python</div>
         <div className="badge badge-lg badge-info">Django</div>
+      </div> */}
+
+      <div className="flex justify-center mt-10 p-10 bg-base-200">
+        <div className="stats stats-vertical lg:stats-horizontal shadow">
+
+          <div className="stat">
+
+            <div className="stat-value">JAVASCRIPT</div>
+
+          </div>
+
+          <div className="stat">
+
+            <div className="stat-value">TYPESCRIPT</div>
+
+          </div>
+          <div className="stat">
+
+            <div className="stat-value">REACT</div>
+
+          </div>
+
+          <div className="stat">
+
+            <div className="stat-value">PYTHON</div>
+
+          </div>
+        </div>
       </div>
-      <About />
+      <div className="flex flex-wrap justify-center mt-10 gap-4 align-middle ">
+        {projects?.map((project) => (
+          <Card key={project.title}
+            title={project.title}
+            liveDemo={project.liveDemo}
+            imagePath={project.imagePath}
+            description={project.description} />
+        ))}
+      </div>
     </div>
   )
 }
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
-function Card() {
-  return (
-    <div>Card</div>
-  )
+type project = {
+    imagePath: string
+    title: string
+    description: string
+    liveDemo: string
+    repoLink?: string
+}
+
+
+const Card: React.FC<project> = (props) => {
+    return (
+        <div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className='h-20'><img src={props.imagePath} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{props.title}</h2>
+                    <p>{props.description}</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary"><a href={props.liveDemo}>Demo</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Card
