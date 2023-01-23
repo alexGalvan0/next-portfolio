59f402d75bcfd684474680dc72f653fcb3641082
Alex <galvan.alex121@gmail.com>
2023-01-22 23:44:34 -0500

    footer again

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,15 +4,18 @@ import Form from './Form'
 function Footer() {
     return (
         <div>
-            <footer className="footer grid-rows-1 p-10 bg-neutral text-neutral-content justify-center">
-                <div>
+            <footer className="footer  p-10 bg-neutral text-neutral-content footer-center">
+                <div className='flex '>
+
                     <span className="footer-title">Services</span>
                     <a className="link link-hover">Branding</a>
                     <a className="link link-hover">Design</a>
                     <a className="link link-hover">Marketing</a>
                     <a className="link link-hover">Advertisement</a>
+
+                    <Form />
                 </div>
-                <Form />
+
             </footer>
         </div>
     )
